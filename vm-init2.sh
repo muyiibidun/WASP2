f6a4939ecd3668583342c1284af8d9cac59f6428
Olumuyiwa Ibidunmoye <muyi@cs.umu.se>
2017-04-20 14:53:36 +0200

    Update

--- vm-init2.sh
+++ vm-init2.sh
@@ -1,32 +1,27 @@
 #!/bin/sh
 
 # set hostname 
-sudo echo “waspymq” > /etc/hostname
+sudo echo waspymq > /etc/hostname
+sudo sed -i “s/localhost/waspymq/g” /etc/hosts
 
-# Install some packages
-sudo apt-get -y update
-sudo apt-get install -y python-dev
-sudo apt-get install -y python-pip
-
-# Install RABBITMQ
 # insert rabbitmq APT repo to local source list
 echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
 
 # add rabbitmq keys to trusted key list 
 wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
 
-# update package list
+# install some dependencies
 sudo apt-get -y update
+sudo apt-get install -y python-dev
+sudo apt-get install -y python-pip
 
 # install rabbitmq package
 sudo apt-get install -y rabbitmq-server
+sudo apt-get install python-pika
 
 # stop and start
-sudo rabbitmqctl stop
-sudo rabbitmq-server start &
-
-# running test hello-world program
-sudo apt-get install python-pika
+# sudo rabbitmqctl stop
+# sudo rabbitmq-server start &
 
 # prepare directory 
 mkdir /usr/local/rabbitmq-test
