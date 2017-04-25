dd118a90e8d5b7c75968bee0c41489bdcdadf053
Olumuyiwa Ibidunmoye <muyi@cs.umu.se>
2017-04-25 18:54:06 +0200

    Update

--- receiver.py
+++ receiver.py
@@ -10,14 +10,12 @@ def receive(connection_info=None):
     qname = "wasp"
     credentials = pika.PlainCredentials(connection_info["username"], connection_info["password"])
     connection = pika.BlockingConnection(pika.ConnectionParameters(connection_info["server"],connection_info["port"],'/',credentials))
+
     channel = connection.channel()
-
 	channel.queue_declare(queue=qname)
-
 	channel.basic_consume(callback, queue=qname, no_ack=True)
-
-	print(' [*] Waiting for messages. To exit press CTRL+C')
-	channel.start_consuming()
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 
 if __name__=="__main__":
