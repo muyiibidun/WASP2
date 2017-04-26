3a34ece801a2d1401ae885c0743844daae8154a9
Olumuyiwa Ibidunmoye <muyi@cs.umu.se>
2017-04-26 12:48:20 +0200

    New updates

--- backend.sh
+++ backend.sh
@@ -4,24 +4,15 @@
 sudo echo waspmq-backend > /etc/hostname
 sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 waspmq-backend/g" /etc/hosts
 
-# insert rabbitmq APT repo to local source list
-echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-
-# add rabbitmq keys to trusted key list 
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-
 # install some dependencies
 sudo apt-get -y update
 sudo apt-get install -y python-dev
 sudo apt-get install -y python-pip
-
-# install rabbitmq package
-sudo apt-get install -y rabbitmq-server
 sudo apt-get install -y python-pika
 
 # prepare directory 
-mkdir /usr/local/rabbitmq-test
-cd /usr/local/rabbitmq-test
+mkdir /usr/local/waspmq
+cd /usr/local/waspmq
 
 # echo "Cloning repo with WASP2"
 git clone https://github.com/muyiibidun/WASP2.git
--- waspmq.sh
+++ waspmq.sh
@@ -4,19 +4,38 @@
 sudo echo waspmq > /etc/hostname
 sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 waspmq/g" /etc/hosts
 
-# insert rabbitmq APT repo to local source list
-echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-
-# add rabbitmq keys to trusted key list 
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-
-# install some dependencies
+# Install some packages
 sudo apt-get -y update
 sudo apt-get install -y python-dev
 sudo apt-get install -y python-pip
 
-# install rabbitmq package
-sudo apt-get install -y rabbitmq-server
+
+#Install Erlang (the RabbitMQ runtime)--download and add Erlang to APT repository
+sudo wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
+sudo apt-get update
+
+#Install Erlang
+sudo apt-get -y install socat erlang-nox=1:19.3-1
+
+#Download the official RabbitMQ 3.6.9 .deb installer package (check the official installation guide for more: http://www.rabbitmq.com/install-debian.html)
+sudo wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.9/rabbitmq-server_3.6.9-1_all.deb
+
+#Install the package using dpkg
+sudo dpkg -i rabbitmq-server_3.6.9-1_all.deb
+
+
+# enable the RabbitMQ service
+sudo update-rc.d rabbitmq-server enable
+
+
+#Start  the RabbitMQ service 
+sudo service rabbitmq-server start
+
+#To stop the service use the command
+# sudo service rabbitmq-server stop
+
+# Install python pika 
 sudo apt-get install -y python-pika
 
 # create users and set privileges to enable remote connection
@@ -24,9 +43,4 @@ rabbitmqctl add_user test test
 rabbitmqctl set_user_tags test administrator
 rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
 
-# prepare directory 
-mkdir /usr/local/rabbitmq-test
-cd /usr/local/rabbitmq-test
 
-# echo "Cloning repo with WASP2"
-git clone https://github.com/muyiibidun/WASP2.git
